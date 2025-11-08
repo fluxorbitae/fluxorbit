c8e50aea95b67f93541e5e996730cc503d5202fe
fluxorbit <eladevrim65@gmail.com>
2025-11-08 06:26:32 +0300

    rr
    rr

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+module.exports = { reactStrictMode: true };
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  reactStrictMode: true,
+};
+
+export default nextConfig;
